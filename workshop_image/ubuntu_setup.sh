8233c012852387ac06779d4ed0b9e82366461ca6
Paul Kent <kentpr@ornl.gov>
2021-09-27 12:01:08 -0400

    Cleanup + PySCF ahead of QMCPACK

--- workshop_image/ubuntu_setup.sh
+++ workshop_image/ubuntu_setup.sh
@@ -79,7 +79,8 @@ if [ ! -e $HOME/apps ]; then
     mkdir $HOME/apps
 fi
 
-## QP install ahead of other apps
+#
+## QP (OUT OF DATE)
 #sudo apt-get -y install ninja-build m4 unzip
 #sudo apt-get -y install python # QP dependencies assume python availability
 #cd $HOME/apps
@@ -111,143 +112,6 @@ fi
 #    sed -i s/"  read_wf = .False."/"  \!read_wf = .False."/g    src/determinants/determinants.irp.f
 #    ninja
 #fi
-#
-#echo --- Intel files setup `date`
-## Setup Intel MKL and MPI
-## Instructions from https://software.intel.com/en-us/articles/installing-intel-free-libs-and-python-apt-repo
-#wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
-#sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
-#rm -f GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
-##Full Intel MKL+Python products 
-#sudo wget https://apt.repos.intel.com/setup/intelproducts.list -O /etc/apt/sources.list.d/intelproducts.list 
-##MKL:  
-##sudo sh -c 'echo deb https://apt.repos.intel.com/mkl all main > /etc/apt/sources.list.d/intel-mkl.list'  
-##Intel MPI
-##sudo sh -c 'echo deb https://apt.repos.intel.com/mpi all main > /etc/apt/sources.list.d/intel-mpi.list'
-#sudo apt-get -y update
-#sudo apt-get -y install intel-mkl-2019.3-062 #Be patient
-#sudo apt-get -y install intel-mpi-2019.3-062 #Be patient
-
-
-## Setup Intel MKL variables, path
-#source /opt/intel/mkl/bin/mklvars.sh intel64
-## Setup Intel MPI variables, path
-#source /opt/intel/impi/2019.3.199/intel64/bin/mpivars.sh
-#
-#
-#if [ ! -e $HOME/apps ]; then
-#    mkdir $HOME/apps
-#fi
-#cd $HOME/apps
-#
-## HDF5 installation
-#if [ ! -e hdf5-hdf5-1_10_5-gcc-impi/lib/libhdf5.a ]; then   
-#echo --- Installing HDF5 `date`
-#    wget https://github.com/live-clones/hdf5/archive/hdf5-1_10_5.tar.gz
-#    tar xvf hdf5-1_10_5.tar.gz
-#    #XXX rm -f hdf5-1_10_5.tar.gz
-#    mkdir hdf5-hdf5-1_10_5-gcc-impi
-#    cd hdf5-hdf5-1_10_5-gcc-impi/
-#    INSTALLDIR=`pwd`
-#    echo INSTALLDIR=$INSTALLDIR
-#    CC=mpigcc FC=mpif90 ../hdf5-hdf5-1_10_5/configure --enable-shared --enable-parallel --enable-fortran  --prefix=$INSTALLDIR >&configure.out
-#    make >&make.out
-#    make install 
-#    cd ..
-#fi
-
-
-#CC="mpigcc" HDF5_MPI="ON" HDF5_DIR=$HOME/apps/hdf5-hdf5-1_10_5-gcc-impi pip install --user --no-binary=h5py h5py
-#exit 1
-
-#
-# QMCPACK and patched QE setup
-#
-# QMCPACK files are needed for QE for pw2qmcpack.x
-#
-echo --- QMCPACK and QE setup
-if  [ $INSTALL_QMCPACK -eq 1 ]; then
-cd $HOME/apps
-if [ ! -e qmcpack ]; then
-    mkdir qmcpack
-fi
-cd qmcpack
-if [ ! -e qmcpack ]; then
-    git clone https://github.com/QMCPACK/qmcpack.git --depth 1
-    cd qmcpack
-    # For reproducible builds, use a fixed commit or version:
-    #    git checkout f95f17d2abb3cf3304553f3fd54aac3c712a1278
-    #    git checkout v3.11.0
-    cd ..
-else
-    cd qmcpack
-    git pull
-    cd ..
-fi
-
-# QE
-if  [ $INSTALL_QE -eq 1 ]; then
-
-if [ ! -e $HOME/apps/qe-6.8/bin/pw.x ]; then
-    echo --- Patching and Building QE `date`
-    cd $HOME/apps/qmcpack/qmcpack/external_codes/quantum_espresso/
-    ./download_and_patch_qe6.8.sh
-    cd qe-6.8
-    mkdir build; cd build
-    if [ -e $HOME/apps/qe-6.8 ]; then
-	rm -r -f $HOME/apps/qe-6.8
-	mkdir $HOME/apps/qe-6.8
-    fi
-    cmake -DCMAKE_C_COMPILER=mpicc -DCMAKE_Fortran_COMPILER=mpif90 -DCMAKE_INSTALL_PREFIX=$HOME/apps/qe-6.8 ..
-    echo --- Make
-    time make  -j 4
-    make install
-    cd ../..
-    # If successfully installed we remove the whole QE directory tree and archive file
-    if [ -e $HOME/apps/qe-6.8/bin/pw.x ]; then
-	rm -r -f qe-6.8 qe-6.8.tar.gz
-    fi
-    cd $HOME/apps
-fi
-fi
-
-
-cd $HOME/apps/qmcpack
-if [ ! -e bin/qmcpack ]; then
-    echo --- Building QMCPACK `date`
-    if [ -e build ]; then
-	rm -r -f build
-    fi
-    
-    mkdir build
-    cd build
-    cmake -DCMAKE_INSTALL_PREFIX=$HOME/apps/qmcpack -DCMAKE_CXX_COMPILER=mpicxx -DCMAKE_C_COMPILER=mpicc -DQE_BIN=$HOME/apps/qe-6.8/build/bin ../qmcpack/
-    time make -j 4
-    make install
-    ctest -L unit
-    cd ..
-    rm -r -f build
-fi
-
-if [ ! -e bin/qmcpack_complex ]; then
-    echo --- Building QMCPACK Complex `date`
-    if [ -e build_complex ]; then
-	rm -r -f build_complex
-    fi
-    mkdir build_complex
-    cd build_complex
-    cmake -DQMC_COMPLEX=1 -DCMAKE_CXX_COMPILER=mpicxx -DCMAKE_C_COMPILER=mpicc -DQE_BIN=$HOME/apps/qe-6.8/build/bin ../qmcpack/
-    time make -j 4
-    ctest -L unit
-    cp -p bin/qmcpack_complex $HOME/apps/qmcpack/bin
-#    cd ../build/bin
-#    ln -sf ../../build_complex/bin/qmcpack_complex qmcpack_complex
-    cd ..
-    rm -r -f build_complex
-    cd ..
-fi
-
-fi
 
 #
 # PySCF
@@ -340,6 +204,7 @@ cd build
 cmake -DBUILD_LIBCINT=0 -DBUILD_LIBXC=0 -DBUILD_XCFUN=0 -DCMAKE_INSTALL_PREFIX:PATH=$here ..
 make -j 4
 echo --- PySCF build done
+# Setup PATHS so that QMCPACK cmake will detect PySCF
 export PYTHONPATH=$topdir:$PYTHONPATH
 export LD_LIBRARY_PATH=$herelib:$LD_LIBRARY_PATH
 echo export PYTHONPATH=$topdir:\$PYTHONPATH
@@ -349,6 +214,96 @@ else
     echo -- Found existing PySCF installation
 fi
 fi
+
+#
+# QMCPACK and patched QE setup
+#
+# QMCPACK files are needed for QE for pw2qmcpack.x
+#
+echo --- QMCPACK and QE setup
+if  [ $INSTALL_QMCPACK -eq 1 ]; then
+cd $HOME/apps
+if [ ! -e qmcpack ]; then
+    mkdir qmcpack
+fi
+cd qmcpack
+if [ ! -e qmcpack ]; then
+    git clone https://github.com/QMCPACK/qmcpack.git --depth 1
+    cd qmcpack
+    # For reproducible builds, use a fixed commit or version:
+    #    git checkout f95f17d2abb3cf3304553f3fd54aac3c712a1278
+    #    git checkout v3.11.0
+    cd ..
+else
+    cd qmcpack
+    git pull
+    cd ..
+fi
+
+# QE
+if  [ $INSTALL_QE -eq 1 ]; then
+
+if [ ! -e $HOME/apps/qe-6.8/bin/pw.x ]; then
+    echo --- Patching and Building QE `date`
+    cd $HOME/apps/qmcpack/qmcpack/external_codes/quantum_espresso/
+    ./download_and_patch_qe6.8.sh
+    cd qe-6.8
+    mkdir build; cd build
+    if [ -e $HOME/apps/qe-6.8 ]; then
+	rm -r -f $HOME/apps/qe-6.8
+	mkdir $HOME/apps/qe-6.8
+    fi
+    cmake -DCMAKE_C_COMPILER=mpicc -DCMAKE_Fortran_COMPILER=mpif90 -DCMAKE_INSTALL_PREFIX=$HOME/apps/qe-6.8 ..
+    echo --- Make
+    time make  -j 4
+    make install
+    cd ../..
+    # If successfully installed we remove the whole QE directory tree and archive file
+    if [ -e $HOME/apps/qe-6.8/bin/pw.x ]; then
+	rm -r -f qe-6.8 qe-6.8.tar.gz
+    fi
+    cd $HOME/apps
+fi
+fi
+
+
+cd $HOME/apps/qmcpack
+if [ ! -e bin/qmcpack ]; then
+    echo --- Building QMCPACK `date`
+    if [ -e build ]; then
+	rm -r -f build
+    fi
+    
+    mkdir build
+    cd build
+    cmake -DCMAKE_INSTALL_PREFIX=$HOME/apps/qmcpack -DCMAKE_CXX_COMPILER=mpicxx -DCMAKE_C_COMPILER=mpicc -DQE_BIN=$HOME/apps/qe-6.8/build/bin ../qmcpack/
+    time make -j 4
+    make install
+    ctest -L unit
+    cd ..
+    rm -r -f build
+fi
+
+if [ ! -e bin/qmcpack_complex ]; then
+    echo --- Building QMCPACK Complex `date`
+    if [ -e build_complex ]; then
+	rm -r -f build_complex
+    fi
+    mkdir build_complex
+    cd build_complex
+    cmake -DQMC_COMPLEX=1 -DCMAKE_CXX_COMPILER=mpicxx -DCMAKE_C_COMPILER=mpicc -DQE_BIN=$HOME/apps/qe-6.8/build/bin ../qmcpack/
+    time make -j 4
+    ctest -L unit
+    cp -p bin/qmcpack_complex $HOME/apps/qmcpack/bin
+#    cd ../build/bin
+#    ln -sf ../../build_complex/bin/qmcpack_complex qmcpack_complex
+    cd ..
+    rm -r -f build_complex
+    cd ..
+fi
+
+fi
+
 
 #
 # DIRAC
