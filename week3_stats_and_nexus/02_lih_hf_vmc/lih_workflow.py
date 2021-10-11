3b32b05b1e8945a747c0e6853749c55da1aa65f7
Jaron Krogel <krogeljt@ornl.gov>
2021-10-11 15:44:53 -0400

    update workflow

--- week3_stats_and_nexus/02_lih_hf_vmc/lih_workflow.py
+++ week3_stats_and_nexus/02_lih_hf_vmc/lih_workflow.py
@@ -5,7 +5,7 @@ from nexus import ppset
 from nexus import generate_physical_system
 from nexus import generate_pyscf
 from nexus import generate_convert4qmc
-from nexus import generate_qmcpack
+from nexus import generate_qmcpack,read_input,vmc
 
 # Obtain the core count of the local machine (lab only)
 import os
@@ -30,37 +30,36 @@ system = generate_physical_system(
     H         = 1,          # H pseudo Zeff
     )
 
-# perform Hartree-Fock
+# Perform Hartree-Fock
 scf = generate_pyscf(
-    identifier = 'scf',               # log output goes to scf.out
-    path       = 'LiH/hf',            # directory to run in
+    identifier = 'scf',               # Log output goes to scf.out
+    path       = 'LiH/hf',            # Directory to run in
     job        = job(serial=True,app='python3'), 
-    template   = './scf_template.py', # pyscf template file
+    template   = './scf_template.py', # PySCF template file
     system     = system,
-    mole       = obj(                 # used to make Mole() inputs
+    mole       = obj(                 # Used to make Mole() inputs
         ecp      = 'ccecp',
         basis    = 'ccecp-ccpvqz',
         symmetry = True,
         ),
-    save_qmc   = True,                # save wfn data for qmcpack
+    save_qmc   = True,                # Save wfn data for qmcpack
     )
 
-# convert orbitals to QMCPACK format
+# Convert orbitals to QMCPACK format
 c4q = generate_convert4qmc(
     identifier   = 'c4q',
     path         = 'LiH/hf',
     job          = job(cores=1),
     no_jastrow   = True,
-    hdf5         = True,              # use hdf5 format
     dependencies = (scf,'orbitals'),
     )
 
-# collect dependencies relating to orbitals
+# Collect dependencies relating to orbitals
 orbdeps = [(c4q,'particles'), # pyscf changes particle positions
            (c4q,'orbitals' )]
 
 
-# run VMC with HF wavefunction
+# Lab: estimate VMC energy equilibration, mean, variance, and errorbar
 qmc = generate_qmcpack(
     identifier   = 'vmc',
     path         = 'LiH/vmc_hf',
@@ -68,12 +67,65 @@ qmc = generate_qmcpack(
     system       = system,
     pseudos      = 'ccecp',
     jastrows     = [],
-    qmc          = 'vmc',             # vmc run
-    seed         = 42,
+    qmc          = 'vmc',             # VMC run
+    seed         = 42,                # Fix the seed (lab only)
     warmupsteps  = 0,
     blocks       = 200,
-    steps        =   3,
+    steps        = 3,
     timestep     = 0.1,
+    dependencies = orbdeps,
+    )
+
+
+# Lab: estimating VMC autocorrelation time
+qmc = generate_qmcpack(
+    identifier   = 'vmc',
+    path         = 'LiH/vmc_ac',
+    job          = job(cores=cores),
+    system       = system,
+    pseudos      = 'ccecp',
+    jastrows     = read_input('opt.J2.xml',format='qmcpack').get('jastrows'),
+    seed         = 42,                # Fix the seed (lab only)
+    calculations = 9*[                # Perform 9 VMC calculations in sequence
+        vmc(warmupsteps  = 20,
+            blocks       = 200,
+            steps        = 3,
+            timestep     = 0.1,
+            )],
+    dependencies = orbdeps,
+    )
+
+
+# Lab: obtaining more precise estimates via the Central Limit Theorem
+qmc = generate_qmcpack(
+    identifier   = 'vmc_1x',
+    path         = 'LiH/vmc_clt',
+    job          = job(cores=cores),
+    system       = system,
+    pseudos      = 'ccecp',
+    jastrows     = read_input('opt.J2.xml',format='qmcpack').get('jastrows'),
+    qmc          = 'vmc',             # VMC run
+    seed         = 42,                # Fix the seed (lab only)
+    warmupsteps  = 20,
+    blocks       = 400,
+    steps        = 5,
+    timestep     = 0.3,
+    dependencies = orbdeps,
+    )
+
+qmc = generate_qmcpack(
+    identifier   = 'vmc_9x',
+    path         = 'LiH/vmc_clt',
+    job          = job(cores=cores),
+    system       = system,
+    pseudos      = 'ccecp',
+    jastrows     = read_input('opt.J2.xml',format='qmcpack').get('jastrows'),
+    qmc          = 'vmc',             # VMC run
+    seed         = 42,                # Fix the seed (lab only)
+    warmupsteps  = 20,
+    blocks       = 3600,
+    steps        = 5,
+    timestep     = 0.3,
     dependencies = orbdeps,
     )
 
