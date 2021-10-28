3f5c0e4b221204eb76111eb7269c72a9086c93a5
Abdulgani Annaberdiyev <aannabe@ncsu.edu>
2021-10-28 11:42:36 -0700

    Working input for workstation

--- week6_ecps_and_observables/02_ccECP_slides_runs/Sc_atom/workstation/workflow.py
+++ week6_ecps_and_observables/02_ccECP_slides_runs/Sc_atom/workstation/workflow.py
@@ -58,6 +58,29 @@ sims.append(c4q)
 orbdeps = [(c4q,'particles'), # pyscf changes particle positions
            (c4q,'orbitals' ),]
 
+
+### Run VMC without Jastrow for sanity check
+gs_noj = generate_qmcpack(
+    identifier   = 'gs_noj',
+    path         = 'gs_noj',
+    job          = job(cores=cores),
+    system       = system,
+    pseudos        = ['Sc.ccECP.xml'],
+    jastrows     = [],
+    calculations   = [
+        vmc(
+            walkers     =   1,
+            warmupsteps =  20,
+            blocks      =   5,
+            steps       =  50,
+            substeps    =   2,
+            timestep    = 0.7,
+            ),
+        ],
+    dependencies = orbdeps,
+    )
+sims.append(gs_noj)
+
 ##### OPTIMIZATION methods
 linopt1 = linear(
     energy               = 0.0,
@@ -103,86 +126,84 @@ optJ12 = generate_qmcpack(
     )
 sims.append(optJ12)
 
-####### optimize 3-body Jastrow
-#optJ123 = generate_qmcpack(
-#    identifier     = 'optJ123',
-#    path           = 'optJ123',
-#    job            = job(nodes=8, queue='debug', hours=0.5, constraint='knl', presub=my_presub),
-#    system         = system,
-#    J3             = True,
-#    J3_rcut        = 12.0,
-#    pseudos        = ['Sc.ccECP.xml'],
-#    calculations   = [
-#                loop(max=10, qmc=linopt3),
-#        ],
-#    dependencies   = orbdeps+[(optJ12,'jastrow')],
-#    )
-#sims.append(optJ123)
-#
-#t_moves = ['no', 'v0', 'v1', 'v3']
-#locality_run = {}
-#
-##### run DMC with QMCPACK
-#NODES=8
-#CORES_PER_NODE = 68
-#MY_TARGET_WALKERS = 4096
-#
-#for moves in t_moves:
-#    name = 'qmc_' + moves
-#
-#    locality_run[moves] = generate_qmcpack(
-#        identifier   = name,
-#        path         = name,
-#        job          = job(nodes=NODES, queue='regular', hours=0.5, constraint='knl', presub=my_presub),
-#        system       = system,
-#        pseudos        = ['Sc.ccECP.xml'],
-#        jastrows     = [],
-#        calculations   = [ 
-#            vmc(
-#                #walkers     =   1,
-#                walkers     = int(MY_TARGET_WALKERS/(NODES*CORES_PER_NODE)),   # Per MPI
-#                warmupsteps =  20,
-#                blocks      =  50,
-#                steps       =  50,
-#                substeps    =   2,
-#                timestep    = 0.5,
-#                ),
-#            dmc(targetwalkers = MY_TARGET_WALKERS,  # Total walkers
-#                timestep = 0.02,
-#                warmupsteps =  int(5/0.02),
-#                blocks = 100,
-#                steps = int(1.0/0.02),
-#                nonlocalmoves = moves,
-#                checkpoint = 5
-#                ),
-#            dmc(targetwalkers = MY_TARGET_WALKERS,  # Total walkers
-#                timestep = 0.01,
-#                warmupsteps =  int(2/0.01),
-#                blocks = 100,
-#                steps = int(1.0/0.01),
-#                nonlocalmoves = moves,
-#                checkpoint = 5
-#                ),
-#            dmc(targetwalkers = MY_TARGET_WALKERS,  # Total walkers
-#                timestep = 0.005,
-#                warmupsteps =  int(1/0.005),
-#                blocks = 100,
-#                steps = int(1.0/0.005),
-#                nonlocalmoves = moves,
-#                checkpoint = 5
-#                ),
-#            dmc(targetwalkers = MY_TARGET_WALKERS,  # Total walkers
-#                timestep = 0.0025,
-#                warmupsteps =  int(1/0.0025),
-#                blocks = 150,
-#                steps = int(1.0/0.0025),
-#                nonlocalmoves = moves,
-#                checkpoint = 5
-#                ),
-#            ],
-#        dependencies = orbdeps+[(optJ123,'jastrow')],
-#        )
-#    sims.append(locality_run[moves])
+###### optimize 3-body Jastrow
+optJ123 = generate_qmcpack(
+    identifier     = 'optJ123',
+    path           = 'optJ123',
+    job            = job(cores=cores),
+    system         = system,
+    J3             = True,
+    J3_rcut        = 12.0,
+    pseudos        = ['Sc.ccECP.xml'],
+    calculations   = [
+                loop(max=10, qmc=linopt3),
+        ],
+    dependencies   = orbdeps+[(optJ12,'jastrow')],
+    )
+sims.append(optJ123)
+
+t_moves = ['no', 'v0', 'v1']
+locality_run = {}
+
+#### run DMC with QMCPACK
+MY_TARGET_WALKERS = 16   # Small number for presentation purposes. Needs to be at least a few thousand walkers.
+
+for moves in t_moves:
+    name = 'qmc_' + moves
+
+    locality_run[moves] = generate_qmcpack(
+        identifier   = name,
+        path         = name,
+        job          = job(cores=cores),
+        system       = system,
+        pseudos        = ['Sc.ccECP.xml'],
+        jastrows     = [],
+        calculations   = [ 
+            vmc(
+                #walkers     =   1,
+                walkers     = int(MY_TARGET_WALKERS/cores),   # Per MPI
+                warmupsteps =  20,
+                blocks      =   5,
+                steps       =  50,
+                substeps    =   2,
+                timestep    = 0.5,
+                ),
+            dmc(targetwalkers = MY_TARGET_WALKERS,  # Total walkers
+                timestep = 0.02,
+                warmupsteps =  int(5/0.02),
+                blocks = 5,
+                steps = int(1.0/0.02),
+                nonlocalmoves = moves,
+                checkpoint = 5
+                ),
+            dmc(targetwalkers = MY_TARGET_WALKERS,  # Total walkers
+                timestep = 0.01,
+                warmupsteps =  int(2/0.01),
+                blocks = 5,
+                steps = int(1.0/0.01),
+                nonlocalmoves = moves,
+                checkpoint = 5
+                ),
+            dmc(targetwalkers = MY_TARGET_WALKERS,  # Total walkers
+                timestep = 0.005,
+                warmupsteps =  int(1/0.005),
+                blocks = 5,
+                steps = int(1.0/0.005),
+                nonlocalmoves = moves,
+                checkpoint = 5
+                ),
+            dmc(targetwalkers = MY_TARGET_WALKERS,  # Total walkers
+                timestep = 0.0025,
+                warmupsteps =  int(1/0.0025),
+                blocks = 5,
+                steps = int(1.0/0.0025),
+                nonlocalmoves = moves,
+                checkpoint = 5
+                ),
+            ],
+        dependencies = orbdeps+[(optJ123,'jastrow')],
+        )
+    sims.append(locality_run[moves])
 
 run_project(sims)
 
